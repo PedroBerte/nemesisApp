5b9edd851126399643cc24c5c95d08930a3f34bc
PedroBerte <pedrobertelli19@gmail.com>
2022-09-22 23:39:50 -0300

    Remoção arquivo diet.js

--- src/pages/Workouts/Workouts.jsx
+++ src/pages/Workouts/Workouts.jsx
@@ -13,8 +13,6 @@ import StatusBarComponent from "../../components/StatusBarComponent/StatusBarCom
 
 import { useAuthContext } from "../../context/AuthContext";
 
-import WorkoutDaysModal from "./WorkoutDaysModal/WorkoutDaysModal";
-
 import { db } from "../../services/firebase-config";
 import { auth } from "../../services/firebase-config";
 import { doc, getDoc } from "firebase/firestore";
